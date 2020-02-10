e41e80a9d273c9c837147b30ddb92ca599d70767
Mike Koenig <koenigm@seattleu.edu>
2020-02-09 23:09:50 -0800

    Added UT for Update in ItemModel

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -1,10 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Game.Models;
 
 namespace UnitTests.Models
@@ -19,11 +14,59 @@ namespace UnitTests.Models
 
             // Act
             var result = new ItemModel();
-            
+
             // Reset
 
-            // Assert
+            // Assert 
             Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ItemModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            result.Value = 6;
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(6,result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Update_Default_Should_Pass()
+        {
+            // Arrange
+            var dataOriginal = new ItemModel();
+            dataOriginal.Value = 1;
+            
+            var dataNew = new ItemModel();
+            dataNew.Value = 2;
+
+            // Act
+            var result = dataOriginal.Update(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(2, dataOriginal.Value);
         }
     }
 }
