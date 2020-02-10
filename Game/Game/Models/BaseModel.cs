97da3eeea6fd1c009d227d3e64da242c72002b48
Mike Koenig <koenigm@seattleu.edu>
2020-02-09 23:09:34 -0800

    Refactored BaseModel. Update to return bool, updated ItemModel and Score Model to Match.

--- Game/Game/Models/BaseModel.cs
+++ Game/Game/Models/BaseModel.cs
@@ -11,9 +11,9 @@ namespace Game.Models
         // Location to the image for the item.  Will come from the server as a fully qualified URI example:  https://developer.android.com/images/robot-tiny.png
         public string ImageURI { get; set; }
 
-        public virtual void Update(T newData)
+        public virtual bool Update(T newData)
         {
-
+            return true;
         }
     }
 }
