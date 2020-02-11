a9ddc96e66df98dafa991f1d799c39bc2ec214ed
Mike Koenig <koenigm@seattleu.edu>
2020-02-11 10:35:47 -0800

    Added AutoBattleEngine Tests

---
+++ UnitTests/Engine/AutoBattleEngineTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+
+using Game.Engine;
+using Game.Models;
+
+namespace UnitTests.Engine
+{
+    [TestFixture]
+    public class AutoBattleEngineTests
+    {
+        AutoBattleEngine Engine;
+
+        [SetUp]
+        public void Setup()
+        {
+            Engine = new AutoBattleEngine();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void AutoBattleEngine_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_RunAutoBattle_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.RunAutoBattle();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true,result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_GetScoreValue_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.GetScoreValue();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_GetScoreObject_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.GetScoreObject();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_GetRoundsValue_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.GetRoundsValue();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void AutoBattleEngine_GetResultsOutput_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine.GetResultsOutput();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("done", result);
+        }
+
+    }
+}
