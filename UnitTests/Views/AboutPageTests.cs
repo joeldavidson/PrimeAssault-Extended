b4fc0972b104d1cbaa2a113056bed93ff3e84f1a
Mike Koenig <koenigm@seattleu.edu>
2020-02-10 20:42:49 -0800

    Adding UT for AboutPage

---
+++ UnitTests/Views/AboutPageTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class AboutPageTests
+    {
+        [Test]
+        public void AboutPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            var app = new App();
+
+            // Act
+            var result = new AboutPage();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
