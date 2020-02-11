0d347210bdfd5c60eb5a457fd36b4fd51d54b562
Mike Koenig <koenigm@seattleu.edu>
2020-02-11 11:16:58 -0800

    Adding AutoBattle Prepare Region to match slides....

--- Game/Game/Engine/AutoBattleEngine.cs
+++ Game/Game/Engine/AutoBattleEngine.cs
@@ -28,13 +28,14 @@ namespace Game.Engine
         // Output Score
         #endregion Algrorithm
 
+            /*
         #region RunAutoBattle
         public async Task<bool> RunAutoBattle()
         {
 
             // Auto Battle, does all the steps that a human would do.
 
-            // Perpare for Battle
+            // Prepare for Battle
 
             // Picks 6 Characters
             // Start
@@ -54,6 +55,7 @@ namespace Game.Engine
             return true;
         }
         #endregion RunAutoBattle
+    */
 
         #region ScoreInformation
         public int GetScoreValue() { return 0; }
@@ -61,5 +63,32 @@ namespace Game.Engine
         public int GetRoundsValue() { return 0; }
         public string GetResultsOutput() { return "done"; }
         #endregion ScoreInformation
+
+        #region RunAutoBattlePrepare
+        public async Task<bool> RunAutoBattle()
+        {
+
+            // Auto Battle, does all the steps that a human would do.
+
+            // Prepare for Battle
+
+            // Picks 6 Characters
+            // Start
+            // Initialize the Rounds
+
+            // Fight
+
+            // Fight Loop. Continue until Game is Over...
+            bool GameContinueCondition = false;
+            do
+            {
+                // Do the turn...
+                // If the round is over start a new one...
+            } while (GameContinueCondition);
+
+            // Wrap up
+            return true;
+        }
+        #endregion RunAutoBattlePrepare
     }
 }
