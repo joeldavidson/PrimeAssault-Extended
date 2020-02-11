65372b9810bf2aeed0bf78f735a10d58458496b5
Mike Koenig <koenigm@seattleu.edu>
2020-02-11 10:59:34 -0800

    Added AutoBattlePage and UT

---
+++ Game/Game/Views/Battle/AutoBattlePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AutoBattlePage : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public AutoBattlePage ()
+		{
+			InitializeComponent ();
+		}
+
+		public async void AutobattleButton_Clicked(object sender, EventArgs e)
+		{
+			// Call into Auto Battle from here to do the Battle...
+			BattleMessageValue.Text = "Done";
+		}
+	}
+}
