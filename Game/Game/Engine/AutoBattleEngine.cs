21731fd5f919b7eebe07fd18aa57973196f0736d
Mike Koenig <koenigm@seattleu.edu>
2020-02-11 08:50:36 -0800

    Added AutoBattleEngine Class

---
+++ Game/Game/Engine/AutoBattleEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Engine
+{
+    class AutoBattleEngine
+    {
+        // Pick 6 Characters
+        // Initialize the Battle
+        // Start a Round
+        // Loop in the round each turn
+        // If Round is over, Start New Round
+        // Check end state of round/game
+        // Get Score
+        // Save Score
+        // Output Score
+    }
+}
