eb6b9c9092be8d3edabe4e358be9463ca3e18afe
Mike Koenig <koenigm@seattleu.edu>
2020-02-10 21:53:08 -0800

    Added UTs for RoundOverPage

--- Game/Game/Views/Battle/RoundOverPage.xaml.cs
+++ Game/Game/Views/Battle/RoundOverPage.xaml.cs
@@ -23,7 +23,7 @@ namespace Game.Views
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		async void NextButton_Clicked(object sender, EventArgs e)
+		public async void NextButton_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PopModalAsync();
 		}
@@ -33,7 +33,7 @@ namespace Game.Views
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		async void PickItems_Clicked(object sender, EventArgs e)
+		public async void PickItems_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushModalAsync(new PickItemsPage());
 		}
---
+++ UnitTests/Views/Battle/RoundOverPageTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class RoundOverPageTests
+    {
+        App app;
+        RoundOverPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new RoundOverPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void RoundOverPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void RoundOverPage_NextButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.NextButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void RoundOverPage_PickItems_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.PickItems_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
