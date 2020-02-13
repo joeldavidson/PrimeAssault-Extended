b3baae02f5c95a6ea0db432ee15fad29f7684c11
Mike Koenig <koenigm@seattleu.edu>
2020-02-12 23:30:07 -0800

    Added Test to the UT for the mock database to get UTs to 100% coverage.

--- UnitTests/TaskExtensionTests.cs
+++ UnitTests/TaskExtensionTests.cs
@@ -59,6 +59,53 @@ namespace UnitTests.Views.Game
             // Assert
             Assert.AreEqual(true, true);
         }
+
+        [Test]
+        public void TestDatabaseService_WipeDataListAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(() => DataSource_SQL.WipeDataListAsync());
+        }
+
+        //private string Task()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        [Test]
+        public void TestDatabaseService_CreateAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(async () => await DataSource_SQL.CreateAsync(null));
+        }
+
+        [Test]
+        public void TestDatabaseService_ReadAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(async () => await DataSource_SQL.ReadAsync(null));
+        }
+
+        [Test]
+        public void TestDatabaseService_UpdateAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(async () => await DataSource_SQL.UpdateAsync(null));
+        }
+
+        [Test]
+        public void TestDatabaseService_DeleteAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(async () =>  await DataSource_SQL.DeleteAsync(null));
+        }
+
+        [Test]
+        public void TestDatabaseService_IndexAsync_Should_Throw_Excpetion()
+        {
+            IDataStore<ItemModel> DataSource_SQL = new TestDatabaseService<ItemModel>(false);
+            Assert.ThrowsAsync<NotImplementedException>(async () => await DataSource_SQL.IndexAsync());
+        }
     }
 
     public class TestDatabaseService<T> : IDataStore<T> where T : new()
