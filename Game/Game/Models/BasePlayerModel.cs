4272fb4adc305b9c7c46ff932f7916736d0b9482
Mike Koenig <koenigm@seattleu.edu>
2020-02-13 11:52:02 -0800

    Refactoring CharaterModel to use a BasePlayerModel

---
+++ Game/Game/Models/BasePlayerModel.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Models
+{
+    public class BasePlayerModel<T> : BaseModel<PlayerInfo>
+    {
+        #region GameEngineAttributes
+        // TurnOrder
+        public int Order;
+
+        // Guid of the original data it links back to the ID, used in Game Engine
+        public string Guid;
+
+        // alive status, !alive will be removed from the list
+        public bool Alive;
+
+        // The type of player, character comes before monster
+        public PlayerTypeEnum PlayerType;
+
+        // Finally if all of the above are the same, sort based on who was loaded first into the list...
+        public int ListOrder;
+
+        #endregion GameEngineAttributes
+
+        // Sorting Order is :  Speed, Level, ExperiencePoints, PlayerType, Name, ListOrder
+
+        // Total speed, including level and items
+        public int Speed;
+
+        // Level of character or monster
+        public int Level;
+
+        // The experience points the player has used in sorting ties...
+        public int ExperiencePoints;
+
+        // Current Health
+        public int CurrentHealth;
+
+        // Max Health
+        public int MaxHealth;
+
+        public int ExperienceTotal = 0;
+
+        // The defense score, to be used for defending against attacks
+        public int Defense { get; set; }
+
+        // The Attack score to be used when attacking
+        public int Attack { get; set; }
+
+        // Item is a string referencing the database table
+        public string Head { get; set; }
+
+        // Feet is a string referencing the database table
+        public string Feet { get; set; }
+
+        // Necklasss is a string referencing the database table
+        public string Necklass { get; set; }
+
+        // PrimaryHand is a string referencing the database table
+        public string PrimaryHand { get; set; }
+
+        // Offhand is a string referencing the database table
+        public string OffHand { get; set; }
+
+        // RightFinger is a string referencing the database table
+        public string RightFinger { get; set; }
+
+        // LeftFinger is a string referencing the database table
+        public string LeftFinger { get; set; }
+
+        public int GetAttack() { return 0; }
+        public int GetDefense() { return 0; }
+        public int GetSpeed() { return 0; }
+        public int GetHealthCurrent() { return CurrentHealth; }
+        public int GetHealthMax() { return MaxHealth; }
+        public int GetDamageRollValue() { return 10; }
+
+
+        // Take Damage
+        // If the damage recived, is > health, then death occurs
+        // Return the number of experience received for this attack 
+        // monsters give experience to characters.  Characters don't accept expereince from monsters
+        public bool TakeDamage(int damage)
+        {
+            if (damage <= 0)
+            {
+                return false;
+            }
+
+            CurrentHealth = CurrentHealth - damage;
+            if (CurrentHealth <= 0)
+            {
+                CurrentHealth = 0;
+
+                // Death...
+                CauseDeath();
+            }
+
+            return true;
+        }
+
+        // Death
+        // Alive turns to False
+        public bool CauseDeath()
+        {
+            Alive = false;
+            return Alive;
+        }
+
+        public List<ItemModel> DropAllItems() { return new List<ItemModel>(); }
+        public string FormatOutput() { return ""; }
+        public bool AddExperience(int newExperience) { return true; }
+        public ItemModel AddItem(ItemLocationEnum itemlocation, string itemID) { return new ItemModel(); }
+        public int CalculateExperienceEarned(int damage) { return 0; }
+        public ItemModel GetItem(string itemString) { return new ItemModel(); }
+        public ItemModel GetItemByLocation(ItemLocationEnum itemLocation)
+        {
+            switch (itemLocation)
+            {
+                case ItemLocationEnum.Head:
+                    return GetItem(Head);
+
+                case ItemLocationEnum.Necklass:
+                    return GetItem(Necklass);
+
+                case ItemLocationEnum.PrimaryHand:
+                    return GetItem(PrimaryHand);
+
+                case ItemLocationEnum.OffHand:
+                    return GetItem(OffHand);
+
+                case ItemLocationEnum.RightFinger:
+                    return GetItem(RightFinger);
+
+                case ItemLocationEnum.LeftFinger:
+                    return GetItem(LeftFinger);
+
+                case ItemLocationEnum.Feet:
+                    return GetItem(Feet);
+            }
+
+            return null;
+        }
+    }
+}
--- Game/Game/Models/CharacterModel.cs
+++ Game/Game/Models/CharacterModel.cs
@@ -1,123 +1,23 @@
 ﻿using System.Collections.Generic;
+using Game.Models;
 
 namespace Game.Models
 {
-    public class CharacterModel : BaseModel<CharacterModel>
+    public class CharacterModel : BasePlayerModel<CharacterModel>
     {
-        public bool Alive = true;
-        public int Level = 0;
-        public int ExperienceTotal = 0;
-
-        // The speed of the character, impacts movement, and initiative
-        public int Speed { get; set; }
-
-        // The defense score, to be used for defending against attacks
-        public int Defense { get; set; }
-
-        // The Attack score to be used when attacking
-        public int Attack { get; set; }
-
-        // Current Health which is always at or below MaxHealth
-        public int CurrentHealth { get; set; }
-
-        // The highest value health can go
-        public int MaxHealth { get; set; }
-
-        // Item is a string referencing the database table
-        public string Head { get; set; }
-
-        // Feet is a string referencing the database table
-        public string Feet { get; set; }
-
-        // Necklasss is a string referencing the database table
-        public string Necklass { get; set; }
-
-        // PrimaryHand is a string referencing the database table
-        public string PrimaryHand { get; set; }
-
-        // Offhand is a string referencing the database table
-        public string OffHand { get; set; }
-
-        // RightFinger is a string referencing the database table
-        public string RightFinger { get; set; }
-
-        // LeftFinger is a string referencing the database table
-        public string LeftFinger { get; set; }
-
-        public int GetAttack() { return 0; }
-        public int GetDefense() { return 0; }
-        public int GetSpeed() { return 0; }
-        public int GetHealthCurrent() { return CurrentHealth; }
-        public int GetHealthMax() { return MaxHealth; }
-        public int GetDamageRollValue() { return 10; }
-        // Take Damage
-        // If the damage recived, is > health, then death occurs
-        // Return the number of experience received for this attack 
-        // monsters give experience to characters.  Characters don't accept expereince from monsters
-        public bool TakeDamage(int damage)
+        public CharacterModel()
         {
-            if (damage <= 0)
-            {
-                return false;
-            }
-
-            CurrentHealth = CurrentHealth - damage;
-            if (CurrentHealth <= 0)
-            {
-                CurrentHealth = 0;
-
-                // Death...
-                CauseDeath();
-            }
-
-            return true;
-        }
-
-        // Death
-        // Alive turns to False
-        public bool CauseDeath()
-        {
-            Alive = false;
-            return Alive;
-        }
-
-        public List<ItemModel> DropAllItems() { return new List<ItemModel>(); }
-        public string FormatOutput() { return ""; }
-        public bool AddExperience(int newExperience) { return true; }
-        public ItemModel AddItem(ItemLocationEnum itemlocation, string itemID) { return new ItemModel(); }
-        public int CalculateExperienceEarned(int damage) { return 0; }
-        public ItemModel GetItem(string itemString) { return new ItemModel(); }
-        public ItemModel GetItemByLocation(ItemLocationEnum itemLocation)
-        {
-            switch (itemLocation)
-            {
-                case ItemLocationEnum.Head:
-                    return GetItem(Head);
-
-                case ItemLocationEnum.Necklass:
-                    return GetItem(Necklass);
-
-                case ItemLocationEnum.PrimaryHand:
-                    return GetItem(PrimaryHand);
-
-                case ItemLocationEnum.OffHand:
-                    return GetItem(OffHand);
-
-                case ItemLocationEnum.RightFinger:
-                    return GetItem(RightFinger);
-
-                case ItemLocationEnum.LeftFinger:
-                    return GetItem(LeftFinger);
-
-                case ItemLocationEnum.Feet:
-                    return GetItem(Feet);
-            }
-
-            return null;
+            PlayerType = PlayerTypeEnum.Character;
         }
     }
 
-    public class MonsterModel : CharacterModel { }
+    public class MonsterModel : BasePlayerModel<MonsterModel>
+    {
+        public MonsterModel()
+        {
+            PlayerType = PlayerTypeEnum.Character;
+        }
+    }
 
     public class PlayerInfo : BaseModel<PlayerInfo>
     {
