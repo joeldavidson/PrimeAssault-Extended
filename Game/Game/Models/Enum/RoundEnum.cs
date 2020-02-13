382b3ef6ccaa42d002680d65315db30c22fd0404
Mike Koenig <koenigm@seattleu.edu>
2020-02-13 11:25:03 -0800

    Added RoundEnum class

---
+++ Game/Game/Models/Enum/RoundEnum.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// The Conditions a round can have
+    /// </summary>
+    public enum RoundEnum 
+    { 
+        Unknown = 0, 
+        NextTurn = 1, 
+        NewRound = 2, 
+        GameOver = 3, 
+    }
+}
