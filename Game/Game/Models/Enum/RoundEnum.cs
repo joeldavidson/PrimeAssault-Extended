da56184a6b59580cddec435252071ddc222e23c7
Mike Koenig <koenigm@seattleu.edu>
2020-02-16 14:37:08 -0800

    Updated Comments

--- Game/Game/Models/Enum/RoundEnum.cs
+++ Game/Game/Models/Enum/RoundEnum.cs
@@ -6,9 +6,16 @@ namespace Game.Models
     /// </summary>
     public enum RoundEnum 
     { 
+        // Invalid State
         Unknown = 0, 
+
+        // Next Turn, monsters and Characters still alive
         NextTurn = 1, 
+
+        // New Round, Monsters dead
         NewRound = 2, 
+
+        // Game Over, characters dead
         GameOver = 3, 
     }
 }
