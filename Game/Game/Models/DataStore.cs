78971ed59a13a0bfd21199da43eb73fa6dc44247
Mike Koenig <koenigm@seattleu.edu>
2020-02-16 14:37:28 -0800

    Updated Comments

--- Game/Game/Models/DataStore.cs
+++ Game/Game/Models/DataStore.cs
@@ -4,8 +4,13 @@ namespace Game.Models
     // What data store will be used.  
     public enum DataStoreEnum
     {
+        // Unknown is not a valid store
         Unknown = 0,
+
+        // Use SQLite and save to disk
         SQL = 1,
+
+        // In Memory Store
         Mock = 2
     }
 }
