12f428bc4788f7a3934ff8d901e71bafae8dce73
Mike Koenig <koenigm@seattleu.edu>
2020-02-16 14:37:22 -0800

    Updated Comments

--- Game/Game/Models/CharacterModel.cs
+++ Game/Game/Models/CharacterModel.cs
@@ -1,8 +1,18 @@
 ﻿
 namespace Game.Models
 {
+    /// <summary>
+    /// Characters
+    /// 
+    /// Derive from BasePlayerModel
+    /// </summary>
     public class CharacterModel : BasePlayerModel<CharacterModel>
     {
+        /// <summary>
+        /// Default character
+        /// 
+        /// Gets a type, guid, name and description
+        /// </summary>
         public CharacterModel()
         {
             PlayerType = PlayerTypeEnum.Character;
