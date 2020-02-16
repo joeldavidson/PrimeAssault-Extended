b7960c12e5eea1602230eca72d00e9ee8493114b
Mike Koenig <koenigm@seattleu.edu>
2020-02-16 14:37:00 -0800

    Updated Comments

--- Game/Game/Models/Enum/MenuItemEnum.cs
+++ Game/Game/Models/Enum/MenuItemEnum.cs
@@ -5,11 +5,22 @@
     /// </summary>
     public enum MenuItemEnum
     {
-        Battle,
-        Village,
-        Score,
-        Items,
-        Game,
-        About
+        // Battle Page
+        Battle = 1,
+
+        // Village
+        Village = 2,
+
+        // Score - Remove
+        Score = 3,
+
+        // Items - Remove
+        Items = 4,
+
+        // Game
+        Game = 5,
+
+        // About
+        About = 6
     }
 }
