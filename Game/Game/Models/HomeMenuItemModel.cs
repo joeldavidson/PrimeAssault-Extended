f1c4489c8134ae26dfe160d738868afd577330ab
Mike Koenig <koenigm@seattleu.edu>
2020-02-16 14:37:33 -0800

    Updated Comments

--- Game/Game/Models/HomeMenuItemModel.cs
+++ Game/Game/Models/HomeMenuItemModel.cs
@@ -5,8 +5,10 @@
     /// </summary>
     public class HomeMenuItemModel
     {
+        // The Current Menu ID
         public MenuItemEnum Id { get; set; }
 
+        // Title for the Page
         public string Title { get; set; }
     }
 }
