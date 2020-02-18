63f9f4e5edbed7492ffd77446bbbe97e1f977e68
Mike Koenig <koenigm@seattleu.edu>
2020-02-18 11:26:57 -0800

    Added DataSetHelper for data Warmup

---
+++ Game/Game/Helpers/DataSetsHelper.cs
@@ -0,0 +1,13 @@
+﻿using Game.ViewModels;
+
+namespace Game.Helpers
+{
+    static public class DataSetsHelper
+    {
+        static public void WarmUp()
+        {
+            ScoreIndexViewModel.Instance.GetCurrentDataSource();
+            ItemIndexViewModel.Instance.GetCurrentDataSource();
+        }
+    }
+}
