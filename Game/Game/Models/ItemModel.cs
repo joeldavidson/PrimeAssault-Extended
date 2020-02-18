8e0ea3699c9f4819795bb00fe90e425ebe9455ac
Mike Koenig <koenigm@seattleu.edu>
2020-02-18 11:28:30 -0800

    Updated ItemModel (removed guid)

--- Game/Game/Models/ItemModel.cs
+++ Game/Game/Models/ItemModel.cs
@@ -32,9 +32,6 @@ namespace Game.Models
 
         // The Value item modifies.  So a ring of Health +3, has a Value of 3
         public int Value { get; set; } = 0;
-
-        //// Guid, passed from the server
-        //public string Guid{ get; set; } = "";
         
         //// Count of how many
         //public int Count { get; set; } = 1;
