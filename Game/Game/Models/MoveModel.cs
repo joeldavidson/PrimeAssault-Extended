d4eb5843da831d5af7896a7c9f13fb9ec9ed55c7
Joel Davidson <davids24@seattleu.edu>
2020-02-18 16:39:20 -0800

    Added default MoveModel and attributes implementation

--- Game/Game/Models/MoveModel.cs
+++ Game/Game/Models/MoveModel.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace PrimeAssault.Models
 {
-    class MoveModel
+    class MoveModel : BaseModel<MoveModel>
     {
+        public int attack { get; set; } = 1;
+
+        public string type { get; set; } = "melee";
+
+        public MoveModel()
+        {
+            Name = "Hard Knuckles";
+            Description = "An extra powerful punch.";
+        }
+
+
+
     }
 }
