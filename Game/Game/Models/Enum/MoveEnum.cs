ea3e1e712b00049ac60985a282a38e1696674c09
Joel Davidson <davids24@seattleu.edu>
2020-02-18 16:24:26 -0800

    Set up code for implementation of Moves

---
+++ Game/Game/Models/Enum/MoveEnum.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PrimeAssault.Models
+{
+    //<summary>
+    //Valid moves
+    //</summary>
+    public enum MoveEnum
+    {
+        //Ranged attack
+        Crackshot = 0,
+
+        //Physical attack
+        Iron_Grip = 1,
+
+    }
+}
