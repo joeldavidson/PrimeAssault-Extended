ecf9c34c30c7d2f587b9ef0d632b0e41af3af7f3
Joel Davidson <davids24@seattleu.edu>
2020-02-22 14:46:39 -0800

    Added proper documentation to MoveModel

--- Game/Game/Models/MoveModel.cs
+++ Game/Game/Models/MoveModel.cs
@@ -4,12 +4,21 @@ using System.Text;
 
 namespace PrimeAssault.Models
 {
+    /// <summary>
+    /// Basic structure of a move
+    /// </summary>
     public class MoveModel : BaseModel<MoveModel>
     {
+        //The size of the dice to be rolled by this attack which will be added to the normal attack stat
         public int attack { get; set; } = 1;
 
+        //Can be melee or ranged, melee attacks can only be used with a melee weapon in hand and ranged attacks can only be used with a ranged weapon
         public string type { get; set; } = "melee";
 
+        //determines who the attack targets, friend of foe (to facilitate healing)
+        public string target { get; set; } = "foe";
+
+        //constructs with default value for a move
         public MoveModel()
         {
             Name = "Hard Knuckles";
