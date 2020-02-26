4b51f6c73f60a90e648fc2c633cc49a1cb44b617
Marco Rodriguez <48032991+Mangoslap@users.noreply.github.com>
2020-02-25 23:39:28 -0800

    Added a max health label to display when character's level is changed in CharCreatepage

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using PrimeAssault.Helpers;
+using PrimeAssault.Models;
+using PrimeAssault.ViewModels;
+
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PrimeAssault.Models;
-using PrimeAssault.ViewModels;
+
 using System.ComponentModel;
 
 using Xamarin.Forms;
@@ -14,6 +18,7 @@ namespace PrimeAssault.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
+
 
     /// <summary>
     /// Create Char
@@ -34,6 +39,64 @@ namespace PrimeAssault.Views
             data.Data = new CharacterModel();
 
             BindingContext = this.ViewModel = data;
+
+            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
+            {
+                LevelPicker.Items.Add(i.ToString());
+            }
+            this.ViewModel.Data.Level = 1;
+
+            UpdatePageBindingContext();
+        }
+
+        /// <summary>
+        /// Redo the Binding to cause a refresh
+        /// </summary>
+        /// <returns></returns>
+        public bool UpdatePageBindingContext()
+        {
+            // Temp store off the Level
+            var level = this.ViewModel.Data.Level;
+
+            // Clear the Binding and reset it
+            BindingContext = null;
+            BindingContext = this.ViewModel;
+
+            // This resets the Picker to -1 index, need to reset it back
+            ViewModel.Data.Level = level;
+            LevelPicker.SelectedIndex = ViewModel.Data.Level - 1;
+
+            ManageHealth();
+
+
+            return true;
+        }
+
+        /// <summary>
+        /// The Level selected from the list
+        /// Need to recalculate Max Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void Level_Changed(object sender, EventArgs args)
+        {
+            // Change the Level
+            ViewModel.Data.Level = LevelPicker.SelectedIndex + 1;
+
+            ManageHealth();
+        }
+
+
+        /// <summary>
+        /// Change the Level Picker
+        /// </summary>
+        public void ManageHealth()
+        {
+            // Roll for new HP
+            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
+
+            // Show the Result
+            MaxHealthValue.Text = ViewModel.Data.MaxHealth.ToString();
         }
 
         /// <summary>
@@ -57,4 +120,7 @@ namespace PrimeAssault.Views
             await Navigation.PopModalAsync();
         }
     }
+
+
+
 }
