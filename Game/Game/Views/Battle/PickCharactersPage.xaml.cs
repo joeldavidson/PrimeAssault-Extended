e542518cfad6c42eca2269d472bc42cbc230a86f
Blake Werner <wernerblake@seattleu.edu>
2020-03-01 23:39:25 -0800

    Hooked up character selction

--- Game/Game/Views/Battle/PickCharactersPage.xaml.cs
+++ Game/Game/Views/Battle/PickCharactersPage.xaml.cs
@@ -1,34 +1,156 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PrimeAssault.Models;
+using PrimeAssault.ViewModels;
+using System.Linq;
 
 namespace PrimeAssault.Views
 {
 	/// <summary>
 	/// The Main PrimeAssault Page
 	/// </summary>
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PickCharactersPage : ContentPage
-	{
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		public PickCharactersPage()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickCharactersPage : ContentPage
+    {
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
 
-		/// <summary>
-		/// Jump to the Battle
-		/// 
-		/// Its Modal because don't want user to come back...
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void BattleButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushModalAsync(new NavigationPage(new BattlePage()));
-			await Navigation.PopAsync();
-		}
-	}
+        // Empty Constructor for UTs
+        public PickCharactersPage(bool UnitTest) { }
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public PickCharactersPage()
+        {
+            InitializeComponent();
+
+            BindingContext = EngineViewModel;
+            //BindingContext = EngineViewModel;
+
+            // Clear the Database List and the Party List to start
+            EngineViewModel.PartyCharacterList.Clear();
+
+            UpdateNextButtonState();
+        }
+        
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnDatabaseCharacterItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Manually deselect Character.
+            CharactersListView.SelectedItem = null;
+
+            // Don't add more than the party max
+            if (EngineViewModel.PartyCharacterList.Count() < EngineViewModel.Engine.MaxNumberPartyCharacters)
+            {
+                EngineViewModel.PartyCharacterList.Add(data);
+            }
+
+            UpdateNextButtonState();
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnPartyCharacterItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Manually deselect Character.
+            PartyListView.SelectedItem = null;
+
+            // Remove the character from the list
+            EngineViewModel.PartyCharacterList.Remove(data);
+
+            UpdateNextButtonState();
+        }
+
+        /// <summary>
+        /// Next Button is based on the count
+        /// 
+        /// If no selected characters, disable
+        /// 
+        /// Show the Count of the party
+        /// 
+        /// </summary>
+        private void UpdateNextButtonState()
+        {
+            // If no characters disable Next button
+            BeginBattleButton.IsEnabled = true;
+
+            var currentCount = EngineViewModel.PartyCharacterList.Count();
+            if (currentCount == 0)
+            {
+                BeginBattleButton.IsEnabled = false;
+            }
+
+            PartyCountLabel.Text = currentCount.ToString();
+        }
+
+        /// <summary>
+        /// Jump to the Battle
+        /// 
+        /// Its Modal because don't want user to come back...
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void BattleButton_Clicked(object sender, EventArgs e)
+        {
+            CreateEngineCharacterList();
+
+            await Navigation.PushModalAsync(new NavigationPage(new BattlePage()));
+            await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Clear out the old list and make the new list
+        /// </summary>
+        public void CreateEngineCharacterList()
+        {
+            // Clear the currett list
+            EngineViewModel.Engine.CharacterList.Clear();
+
+            // Load the Characters into the Engine
+            foreach (var data in EngineViewModel.PartyCharacterList)
+            {
+                EngineViewModel.Engine.CharacterList.Add(new PlayerInfoModel(data));
+            }
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Clear the Binding
+            BindingContext = null;
+
+            EngineViewModel.PartyCharacterList.Clear();
+
+            // Force the Binding to Update
+            BindingContext = EngineViewModel;
+
+            UpdateNextButtonState();
+        }
+    }
 }
