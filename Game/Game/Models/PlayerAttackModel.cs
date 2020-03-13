8d60d714c4ac8fe5cd234f935969f5cfa0833558
Joel Davidson <davids24@seattleu.edu>
2020-03-12 22:06:48 -0700

    Created a model which can hold player attack information

---
+++ Game/Game/Models/PlayerAttackModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeAssault.Models
+{
+    class PlayerAttackModel
+    {
+        public PlayerInfoModel Target;
+        public PlayerInfoModel Attacker;
+        public MoveEnum Move;
+
+        public PlayerAttackModel()
+        {
+            Target = null;
+            Attacker = null;
+            Move = MoveEnum.None;
+        }
+    }
+}
